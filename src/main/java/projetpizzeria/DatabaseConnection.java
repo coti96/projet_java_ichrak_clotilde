52f96fc65eeec79a5c0c7939d2042b3f14910dae
coti96 <clotildepaluch@gmail.com>
2024-05-29 13:34:55 +0200

    Fix commit qui reorganise

--- src/main/java/projetpizzeria/DatabaseConnection.java
+++ src/main/java/projetpizzeria/DatabaseConnection.java
@@ -5,19 +5,22 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DatabaseConnection  {
-    public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/pizzeria";
-        String user = "root";
-        String password = "";
+public class DatabaseConnection {
 
+    private String url = "jdbc:mysql://localhost:3306/pizzeria";
+    private String user = "root";
+    private String password = "";
+
+    public Connection connect() {
         try {
             Connection connection = DriverManager.getConnection(url, user, password);
             if (connection != null) {
-                System.out.println("ConnectÃ© Ã  la base de donnÃ©es !");
+                System.out.println("Connecté à la base de données !");
+                return connection;
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return null;
     }
 }
